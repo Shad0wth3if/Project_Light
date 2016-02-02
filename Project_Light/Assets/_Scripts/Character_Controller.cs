cb2bb9812f307139a083a23ba002b139ccd5b124
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2016-02-02 13:59:18 -0800

    camera control prototype

---
+++ Project_Light/Assets/_Scripts/Character_Controller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character_Controller : MonoBehaviour {
+
+    public Transform camLoc;
+    public GameObject target;
+    public GameObject character;
+    public float moveSpeed;
+
+    Unit unit;
+    GameObject targetInstance;
+
+    // Use this for initialization
+    void Start () {
+        unit = character.GetComponent<Unit>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Destroy(targetInstance);
+                targetInstance = Instantiate(target, hitInfo.point, Quaternion.identity) as GameObject;
+                unit.UnitDestination(targetInstance);
+            }
+        }
+	}
+
+    void LateUpdate()
+    {
+        camLoc.position = Vector3.Lerp(transform.position, character.transform.position, moveSpeed);
+    }
+}
---
+++ Project_Light/Assets/_Scripts/Unit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+    public GameObject character;
+
+    GameObject flag;
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        character = this.gameObject;
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == flag)
+        {
+            Destroy(flag);
+        }
+    }
+
+    public void UnitDestination (GameObject recievedFlag)
+    {
+        flag = recievedFlag;
+        agent.SetDestination(recievedFlag.transform.position);
+    }
+}
